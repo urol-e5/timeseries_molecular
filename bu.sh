e7dbcc6ea3df11eb04ce9b7f8fcb3732764420fe
STEVEN <sr320@uw.edu>
2025-08-27 07:24:19 -0700

    lnc REdiscovery for apul

--- bu.sh
+++ bu.sh
@@ -9,7 +9,7 @@
 # --exclude='.*' --exclude='*/.*' \
 # sr320@gannet.fish.washington.edu:/volume1/v1_web/owlshell/bu-github/
 
-rsync -av ../timeseries_molecular \
+rsync -avz ../timeseries_molecular \
 --exclude='*.sam' \
 --exclude='tmp*' \
 --exclude='*gz_C_to_T.fastq' \
@@ -19,7 +19,7 @@ rsync -av ../timeseries_molecular \
 sr320@gannet.fish.washington.edu:/volume1/v1_web/owlshell/bu-github/
 
 
-rsync -av ../timeseries_molecular \
+rsync -avz ../timeseries_molecular \
 --exclude='*.sam' \
 --exclude='tmp*' \
 --exclude='*gz_C_to_T.fastq' \
